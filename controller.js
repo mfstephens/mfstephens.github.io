4254cf7c932b3464046b706da29ac63034909c08
Matt Stephens <mfstep@umich.edu>
2013-07-30 15:39:24 -0400

    Almost have working final product

---
+++ controller.js
@@ -0,0 +1,11 @@
+$("#about").click(function() {
+	$(".content").html("Lorem ipsum dolor sit amet, id vero nulla vituperata pro. Ea eos velit expetendis. Ad vel utinam minimum nostrum. Vim te eros commodo docendi, per ea libris noster accommodare. Est viris omittantur cu, facete adversarium ei usu. At mel nihil delenit eligendi, id mei putant iracundia. Hinc appareat interpretaris sea eu, vel ne solum debitis, vero timeam sea et. At his elitr epicuri, sed altera intellegat constituam id, no quem rebum tacimates eum. Est nusquam mentitum quaerendum ne, at velit numquam interpretaris vix. Vis te eruditi forensibus, illum menandri no nec. Dicam singulis id vim, timeam constituto est ad, pro in libris quodsi platonem. Esse conclusionemque usu cu. Dolorum inimicus neglegentur vis ut, te antiopam adversarium per, usu ei dicta persequeris.");
+});
+
+$("#projects").click(function() {
+	$(".content").html('<div class="project-preview first"> <p> </p> </div> <div class="project-preview last"></div> <div class="project-preview first"></div> <div class="project-preview last">');
+});
+
+$("#contact").click(function() {
+	$(".content").html("Lorem ipsum dolor sit amet, id vero nulla vituperata pro. Ea eos velit expetendis. Ad vel utinam minimum nostrum. Vim te eros commodo docendi, per ea libris noster accommodare. Est viris omittantur cu, facete adversarium ei usu. At mel nihil delenit eligendi, id mei putant iracundia. Hinc appareat interpretaris sea eu, vel ne solum debitis, vero timeam sea et. At his elitr epicuri, sed altera intellegat constituam id, no quem rebum tacimates eum. Est nusquam mentitum quaerendum ne, at velit numquam interpretaris vix. Vis te eruditi forensibus, illum menandri no nec. Dicam singulis id vim, timeam constituto est ad, pro in libris quodsi platonem. Esse conclusionemque usu cu. Dolorum inimicus neglegentur vis ut, te antiopam adversarium per, usu ei dicta persequeris.");
+});
