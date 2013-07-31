98549cbbc6fbd23870a6336af311eaf7e3776894
Matt Stephens <mfstep@umich.edu>
2013-07-31 15:31:22 -0400

    Working on projects page

--- controller.js
+++ controller.js
@@ -1,20 +1,20 @@
-$("#about").click(function() {
-	$("#projects").removeClass("selected-tab");
-	$("#contact").removeClass("selected-tab");
-	$("#about").addClass("selected-tab");
-	$(".content").html(' Lorem ipsum dolor sit amet, id vero nulla vituperata pro. Ea eos velit expetendis. Ad vel utinam minimum nostrum. Vim te eros commodo docendi, per ea libris noster accommodare. Est viris omittantur cu, facete adversarium ei usu. At mel nihil delenit eligendi, id mei putant iracundia. Hinc appareat interpretaris sea eu, vel ne solum debitis, vero timeam sea et. At his elitr epicuri, sed altera intellegat constituam id, no quem rebum tacimates eum. Est nusquam mentitum quaerendum ne, at velit numquam interpretaris vix. Vis te eruditi forensibus, illum menandri no nec. Dicam singulis id vim, timeam constituto est ad, pro in libris quodsi platonem. Esse conclusionemque usu cu. Dolorum inimicus neglegentur vis ut, te antiopam adversarium per, usu ei dicta persequeris. <div class="umich-logo"></div>');
+$("#about-nav").click(function() {
+	$("#projects-nav").removeClass("selected-tab");
+	$("#contact-nav").removeClass("selected-tab");
+	$("#about-nav").addClass("selected-tab");
+	$(".content").html("<img src='Michigan2.png' width='300px' height='200px' style='float: right;''> <p> Hi there. I'm Matt Stephens. I'm a student at the University of Michigan in my third year studying computer science. I like programming, music, and fine dining (especially good beer). </p> <p> I have experience with JavaScript (and all its friends including Backbone, Angular, JQuery, Node, & QUnit), HTML+CSS, and C++. Check out some of my projects to see what I've done with these technologies. </p> <p> Though I am currently employed as a Software Engineering Intern at TechSmith Corporation until August 2013, I'm always looking for fun projects to work on and cool companies to work for. Interested in seeing me struggle to write code? Hit me up. </p>");
 });
 
-$("#projects").click(function() {
-	$("#contact").removeClass("selected-tab");
-	$("#about").removeClass("selected-tab");
-	$("#projects").addClass("selected-tab");
+$(".projects").click(function() {
+	$("#contact-nav").removeClass("selected-tab");
+	$("#about-nav").removeClass("selected-tab");
+	$("#projects-nav").addClass("selected-tab");
 	$(".content").html('<div class="project-preview first"></div> <div class="project-preview"></div> <div class="project-preview last"></div> <div class="project-preview first">');
 });
 
-$("#contact").click(function() {
-	$("#projects").removeClass("selected-tab");
-	$("#about").removeClass("selected-tab");
-	$("#contact").addClass("selected-tab");
-	$(".content").html("Lorem ipsum dolor sit amet, id vero nulla vituperata pro. Ea eos velit expetendis. Ad vel utinam minimum nostrum. Vim te eros commodo docendi, per ea libris noster accommodare. Est viris omittantur cu, facete adversarium ei usu. At mel nihil delenit eligendi, id mei putant iracundia. Hinc appareat interpretaris sea eu, vel ne solum debitis, vero timeam sea et. At his elitr epicuri, sed altera intellegat constituam id, no quem rebum tacimates eum. Est nusquam mentitum quaerendum ne, at velit numquam interpretaris vix. Vis te eruditi forensibus, illum menandri no nec. Dicam singulis id vim, timeam constituto est ad, pro in libris quodsi platonem. Esse conclusionemque usu cu. Dolorum inimicus neglegentur vis ut, te antiopam adversarium per, usu ei dicta persequeris.");
+$(".contact").click(function() {
+	$("#projects-nav").removeClass("selected-tab");
+	$("#about-nav").removeClass("selected-tab");
+	$("#contact-nav").addClass("selected-tab");
+	$(".content").html("<p> Want to talk about beer, fun side-projects, or maybe even hire me? Shoot me an email: </p> <p> email: mfstep@umich.edu Personal email: mfstephens9@gmail.com </p> <p> Interested in learning more about me? Check me out on... </p>");
 });
