e59398794154bc4cfd2611c729ef7947117be149
Matt Stephens <mfstep@umich.edu>
2013-08-01 14:14:07 -0400

    added contact icons

--- controller.js
+++ controller.js
@@ -2,7 +2,7 @@ $(".about").click(function() {
 	$("#projects-nav").removeClass("selected-tab");
 	$("#contact-nav").removeClass("selected-tab");
 	$("#about-nav").addClass("selected-tab");
-	$(".content").html("<img src='Michigan2.png' width='300px' height='200px' style='float: right;''> <p> Hi there. I'm Matt Stephens. I'm a student at the University of Michigan in my third year studying computer science. I like programming, music, and fine dining (especially good beer). </p> <p> I have experience with JavaScript (and all its friends including Backbone, Angular, JQuery, Node, & QUnit), HTML+CSS, and C++. Check out some of my projects to see what I've done with these technologies. </p> <p> Though I am currently employed as a Software Engineering Intern at TechSmith Corporation until August 2013, I'm always looking for fun projects to work on and cool companies to work for. Interested in seeing me struggle to write code? Hit me up. </p>");
+	$(".content").html("<img src='Michigan2.png' width='300px' height='200px' style='float: right;'> <p> Hi there. I'm Matt Stephens. I'm a student at the <a href='http://cs.lsa.umich.edu/' class='link'>University of Michigan</a> in my third year studying computer science. I like programming, music, and good beer. </p> <p> I have experience with JavaScript (and all its friends including Backbone, Angular, jQuery, and Node), HTML+CSS, and C++. If you're interested, you can head over and check out my <input type='button' class='projects link' value='projects'></input> to see some stuff I've worked on. </p> <p> Though I am currently employed for the summer as a software engineering intern at <a href='http://www.techsmith.com' class='link'>TechSmith Corporation</a>, I'm always looking for fun projects to work on. So, wanna make something cool? <input type='button' class='contact link' value='Hit me up'></input>. </p>");
 });
 
 $(".projects").click(function() {
@@ -16,5 +16,5 @@ $(".contact").click(function() {
 	$("#projects-nav").removeClass("selected-tab");
 	$("#about-nav").removeClass("selected-tab");
 	$("#contact-nav").addClass("selected-tab");
-	$(".content").html('<p> Want to talk about beer, fun side-projects, or maybe even hire me? Shoot me an email: </p> <p> <strong>email</strong>: <a class="link" href="mfstep@umich.edu">mfstep@umich.edu</a> </p> <p> <strong>Personal email</strong>: <a class="link href="mfstephens9@gmail.com">mfstephens9@gmail.com</a> </p> <p> Interested in learning more about me? Check me out on... </p>')
+	$(".content").html("<p> Want to talk about beer, side-projects, or maybe even hire me? I'm always looking for stuff to do. </p> <p> <span class='symbol'>roundedemail</span> <span class='symbol'>&#xe437;</span> <span class='symbol'>roundedlinkedin</span> <span class='symbol'>roundedgoogleplus</span> </p>");
 });
