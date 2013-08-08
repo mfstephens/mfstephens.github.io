4a426bb6c8507a12d68a118d91be9f9ad93e56d9
Matt Stephens <mfstep@umich.edu>
2013-08-08 13:24:30 -0400

    broke up into pages and added CNAME

--- controller.js
+++ controller.js
@@ -2,19 +2,16 @@ $(".about").click(function() {
 	$("#projects-nav").removeClass("selected-tab");
 	$("#contact-nav").removeClass("selected-tab");
 	$("#about-nav").addClass("selected-tab");
-	$(".content").html("<img src='Michigan2.png' width='300px' height='200px' style='float: right;'> <p> Hi there. I'm Matt Stephens. I'm a student at the <a href='http://cs.lsa.umich.edu/' class='link'>University of Michigan</a> in my third year studying computer science. I like programming, music, and good beer. </p> <p> I have experience with JavaScript (and all its friends including Backbone, Angular, jQuery, and Node), HTML+CSS, and C++. If you're interested, you can head over and check out my <input type='button' class='projects link' value='projects'></input> to see some stuff I've worked on. </p> <p> Though I am currently employed for the summer as a software engineering intern at <a href='http://www.techsmith.com' class='link'>TechSmith Corporation</a>, I'm always looking for fun projects to work on. So, wanna make something cool? <input type='button' class='contact link' value='Hit me up'></input>. </p>");
 });
 
 $(".projects").click(function() {
 	$("#contact-nav").removeClass("selected-tab");
 	$("#about-nav").removeClass("selected-tab");
 	$("#projects-nav").addClass("selected-tab");
-	$(".content").html('<div class="project-preview first"></div> <div class="project-preview"></div> <div class="project-preview last"></div> <div class="project-preview first">');
 });
 
 $(".contact").click(function() {
 	$("#projects-nav").removeClass("selected-tab");
 	$("#about-nav").removeClass("selected-tab");
 	$("#contact-nav").addClass("selected-tab");
-	$(".content").html("<p> Want to talk about beer, side-projects, or maybe even hire me? I'm always looking for stuff to do. </p> <p> <span class='symbol'>roundedemail</span> <span class='symbol'>&#xe437;</span> <span class='symbol'>roundedlinkedin</span> <span class='symbol'>roundedgoogleplus</span> </p>");
 });
