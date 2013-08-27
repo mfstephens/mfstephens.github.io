3d2177abfee1b693afcab83037c48f9fdda19905
mfstephens <mfstep@umich.edu>
2013-08-27 17:42:18 -0400

    Finalized projects page design

--- projects.js
+++ projects.js
@@ -1,13 +1,11 @@
 var displayControlsById = function(id) {
 	$("#" + id + "-code").css("display", "inline-block");
 	$("#" + id + "-preview").css("display", "inline-block");
-	$("#" + id + "-info").css("display", "inline-block");	
 };
 
 var hideControlsById = function(id) {
 	$("#" + id + "-code").css("display", "none");
 	$("#" + id + "-preview").css("display", "none");
-	$("#" + id + "-info").css("display", "none");
 }
 
 $("#space-pioneers").mouseout(function() {
@@ -25,3 +23,20 @@ $("#calculator").mouseout(function() {
 $("#calculator").mouseover(function() {
 	displayControlsById($(this).attr('id'));
 });
+
+$("#superhaiku").mouseout(function() {
+	hideControlsById($(this).attr('id'));
+});
+
+$("#superhaiku").mouseover(function() {
+	displayControlsById($(this).attr('id'));
+});
+
+$("#melonJS-platformer").mouseout(function() {
+	hideControlsById($(this).attr('id'));
+});
+
+$("#melonJS-platformer").mouseover(function() {
+	displayControlsById($(this).attr('id'));
+});
+
