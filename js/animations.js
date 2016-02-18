4913001856a325091880d171a37924532718ac81
Matt Stephens <mfstephens9@gmail.com>
2016-02-18 13:15:08 -0500

    New design

---
+++ js/animations.js
@@ -0,0 +1,21 @@
+// var controller = new ScrollMagic.Controller();
+
+
+// var scene = new ScrollMagic.Scene({triggerElement: "#main-nav", duration: "40%"})
+// 					.setTween("#my-name", {scale: 0.26})
+// 					.addTo(controller);
+
+// var scene2 = new ScrollMagic.Scene({triggerElement: "#main-nav", duration: 100})
+// 					.setTween(".intro-text", {opacity: 0.0})
+// 					.addTo(controller);	
+
+// scene.on("end", function (event) {
+// 	if($('#navbar-title:visible').length == 0) {
+// 	    $("#navbar-title").show();
+// 	    $("#my-name").hide();
+// 	} else {
+// 	    $("#navbar-title").hide();
+// 	    $("#my-name").show();
+// 	}
+// });
+// Maybe one day...
